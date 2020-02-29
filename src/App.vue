c77ca16f0edba9766b5d5aa6a31087cf275d8b98
Adam Toomey <Adam@Adams-MacBook-Pro.local>
2020-02-29 12:47:54 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-content>
+      <Baseline/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import Baseline from './components/baseline-template';
+
+export default {
+  name: 'App',
+
+  components: {
+    Baseline
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
